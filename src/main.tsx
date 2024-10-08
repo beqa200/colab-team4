aca0c17e9ff1dfee193c639fc7a7c7195948d7a0
nininini <jajanidzenino60@gmail.com>
2024-10-08 20:51:49 +0400

    first commit

---
+++ src/main.tsx
@@ -0,0 +1,18 @@
+import { createRoot } from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import "./index.css";
+import Home from "./pages/Home";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+]);
+
+createRoot(
+  document.getElementById("root")!
+).render(<RouterProvider router={router} />);
